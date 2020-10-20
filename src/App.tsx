56f0ec09417fe61deceb5bbbd123b651b497e3ee
Shailesh Nair <shailesh.nair@grindan.com>
2020-10-20 11:34:15 +0530

    Testing the commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer">
-                    Learn React
+                    Learn React 1
                 </a>
             </header>
         </div>
